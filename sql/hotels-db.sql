6ce3e64a83b609706ebe5477218711d32b21acd0
reallygoodsir <tymur5512@gmail.com>
2024-12-21 20:32:32 +0200

    Added updated DB schema

--- sql/hotels-db.sql
+++ sql/hotels-db.sql
@@ -1,102 +1,223 @@
--- Create the schema
-CREATE SCHEMA IF NOT EXISTS hotels_db;
-USE hotels_db;
+CREATE DATABASE  IF NOT EXISTS `hotels_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hotels_db`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hotels_db
+-- ------------------------------------------------------
+-- Server version	9.0.1
 
--- Create the tables
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- country table
-CREATE TABLE country (
-    country_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
-);
+--
+-- Table structure for table `city`
+--
 
--- city table
-CREATE TABLE city (
-    city_id INT AUTO_INCREMENT PRIMARY KEY,
-    country_id INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    FOREIGN KEY (country_id) REFERENCES country(country_id)
-);
+DROP TABLE IF EXISTS `city`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `city` (
+  `city_id` int NOT NULL AUTO_INCREMENT,
+  `country_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`city_id`),
+  KEY `country_id` (`country_id`),
+  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- street table
-CREATE TABLE street (
-    street_id INT AUTO_INCREMENT PRIMARY KEY,
-    city_id INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    FOREIGN KEY (city_id) REFERENCES city(city_id)
-);
+--
+-- Table structure for table `country`
+--
 
--- customer table
-CREATE TABLE customer (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    phone_number VARCHAR(15) NOT NULL
-);
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `country` (
+  `country_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`country_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- hotel table
-CREATE TABLE hotel (
-    hotel_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
+--
+-- Table structure for table `customer`
+--
 
--- hotel_info table
-CREATE TABLE hotel_info (
-    hotel_info_id INT AUTO_INCREMENT PRIMARY KEY,
-    hotel_id INT NOT NULL,
-    phone_number VARCHAR(15) UNIQUE NOT NULL,
-    has_parking BOOLEAN DEFAULT FALSE,
-    has_wifi BOOLEAN DEFAULT FALSE,
-    has_swimming_pool BOOLEAN DEFAULT FALSE,
-    details TEXT,
-    FOREIGN KEY (hotel_id) REFERENCES hotel(hotel_id)
-);
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(15) NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- hotel_address table
-CREATE TABLE hotel_address (
-    hotel_address_id INT AUTO_INCREMENT PRIMARY KEY,
-    hotel_id INT NOT NULL,
-    country_id INT NOT NULL,
-    city_id INT NOT NULL,
-    street_id INT NOT NULL,
-    FOREIGN KEY (hotel_id) REFERENCES hotel(hotel_id),
-    FOREIGN KEY (country_id) REFERENCES country(country_id),
-    FOREIGN KEY (city_id) REFERENCES city(city_id),
-    FOREIGN KEY (street_id) REFERENCES street(street_id)
-);
+--
+-- Table structure for table `hotel`
+--
 
--- room table
-CREATE TABLE room (
-    room_id INT AUTO_INCREMENT PRIMARY KEY,
-    hotel_id INT NOT NULL,
-    room_number VARCHAR(10) NOT NULL,
-    is_available BOOLEAN DEFAULT TRUE,
-    FOREIGN KEY (hotel_id) REFERENCES hotel(hotel_id)
-);
+DROP TABLE IF EXISTS `hotel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hotel` (
+  `hotel_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`hotel_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- room_info table
-CREATE TABLE room_info (
-    room_info_id INT AUTO_INCREMENT PRIMARY KEY,
-    room_id INT NOT NULL,
-    room_type VARCHAR(50) NOT NULL,
-    price_per_night DECIMAL(10, 2) NOT NULL,
-    adults_capacity INT NOT NULL,
-    children_capacity INT NOT NULL,
-    has_air_conditioning BOOLEAN DEFAULT FALSE,
-    details TEXT,
-    FOREIGN KEY (room_id) REFERENCES room(room_id)
-);
+--
+-- Table structure for table `hotel_address`
+--
 
--- transaction table
-CREATE TABLE transaction (
-    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
-    hotel_id INT NOT NULL,
-    room_id INT NOT NULL,
-    customer_id INT NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    check_in DATE NOT NULL,
-    check_out DATE NOT NULL,
-    FOREIGN KEY (hotel_id) REFERENCES hotel(hotel_id),
-    FOREIGN KEY (room_id) REFERENCES room(room_id),
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
-);
+DROP TABLE IF EXISTS `hotel_address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hotel_address` (
+  `hotel_address_id` int NOT NULL AUTO_INCREMENT,
+  `hotel_id` int NOT NULL,
+  `country_id` int NOT NULL,
+  `city_id` int NOT NULL,
+  `street_id` int NOT NULL,
+  PRIMARY KEY (`hotel_address_id`),
+  KEY `hotel_id` (`hotel_id`),
+  KEY `country_id` (`country_id`),
+  KEY `city_id` (`city_id`),
+  KEY `street_id` (`street_id`),
+  CONSTRAINT `hotel_address_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`),
+  CONSTRAINT `hotel_address_ibfk_2` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`),
+  CONSTRAINT `hotel_address_ibfk_3` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`),
+  CONSTRAINT `hotel_address_ibfk_4` FOREIGN KEY (`street_id`) REFERENCES `street` (`street_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `hotel_info`
+--
+
+DROP TABLE IF EXISTS `hotel_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hotel_info` (
+  `hotel_info_id` int NOT NULL AUTO_INCREMENT,
+  `hotel_id` int NOT NULL,
+  `phone_number` varchar(15) NOT NULL,
+  `has_parking` tinyint(1) DEFAULT '0',
+  `has_wifi` tinyint(1) DEFAULT '0',
+  `has_swimming_pool` tinyint(1) DEFAULT '0',
+  `details` text,
+  PRIMARY KEY (`hotel_info_id`),
+  UNIQUE KEY `phone_number` (`phone_number`),
+  KEY `hotel_id` (`hotel_id`),
+  CONSTRAINT `hotel_info_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `room_id` int NOT NULL AUTO_INCREMENT,
+  `hotel_id` int NOT NULL,
+  `room_number` varchar(10) NOT NULL,
+  `is_available` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`room_id`),
+  KEY `hotel_id` (`hotel_id`),
+  CONSTRAINT `room_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `room_info`
+--
+
+DROP TABLE IF EXISTS `room_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room_info` (
+  `room_info_id` int NOT NULL AUTO_INCREMENT,
+  `room_id` int NOT NULL,
+  `room_type` varchar(50) NOT NULL,
+  `price_per_night` decimal(10,2) NOT NULL,
+  `adults_capacity` int NOT NULL,
+  `children_capacity` int NOT NULL,
+  `has_air_conditioning` tinyint(1) DEFAULT '0',
+  `details` text,
+  PRIMARY KEY (`room_info_id`),
+  KEY `room_id` (`room_id`),
+  CONSTRAINT `room_info_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `street`
+--
+
+DROP TABLE IF EXISTS `street`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `street` (
+  `street_id` int NOT NULL AUTO_INCREMENT,
+  `city_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`street_id`),
+  KEY `city_id` (`city_id`),
+  CONSTRAINT `street_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `hotel_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  `customer_id` int NOT NULL,
+  `total_price` decimal(10,2) NOT NULL,
+  `check_in` date NOT NULL,
+  `check_out` date NOT NULL,
+  `confirmation_number` varchar(37) DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`),
+  UNIQUE KEY `confirmation_number_UNIQUE` (`confirmation_number`),
+  KEY `hotel_id` (`hotel_id`),
+  KEY `room_id` (`room_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`),
+  CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`),
+  CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-21 20:31:47
